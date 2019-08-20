1f0d765395b7e688f52904711ae8ce00ba07b223
Won Jonghwan <wjh1461@naver.com>
2019-08-20 19:46:19 +0900

    보조 클래스 추가
    MathHelper 클래스, UploadBuffer 클래스

---
+++ MathHelper.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <DirectXMath.h>
+
+class MathHelper
+{
+public:
+	template<typename T>
+	static T Clamp(const T& x, const T& low, const T& high)
+	{
+		return x < low ? low : (x > high ? high : x);
+	}
+
+	static DirectX::XMFLOAT4X4 Identity4x4() 
+	{
+		static DirectX::XMFLOAT4X4 I(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f);
+
+		return I;
+	}
+
+	static const float Pi;
+};
+
